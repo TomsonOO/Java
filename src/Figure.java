5b0946becf022afd5391db13db5fd88a97902f45
Tomson <45737544+TomsonOO@users.noreply.github.com>
2022-10-14 10:23:30 +0200

    Add files via upload

---
+++ src/Figure.java
@@ -0,0 +1,16 @@
+abstract public class Figure {
+
+
+    public double calculatedArea(){
+
+        System.out.println("Calculated Area here: ");
+    }
+
+    public double calculatePerimeter()
+    {
+        System.out.println("Calculated Perimeter: ");
+
+    }
+
+
+}
---
+++ src/Triangle.java
@@ -0,0 +1,16 @@
+
+
+class Triangle extends Figure  implements Printable{
+
+    int a, b, c;
+
+    public Triangle(int a, int b, int c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+
+
+
+}
